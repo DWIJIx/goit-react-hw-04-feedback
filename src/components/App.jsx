00a0fb907f23ea7b3b457895404165d1d63a661e
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-04-11 14:16:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,65 @@
-import { Component } from "react";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { GlobalStyle } from "./GlobalStyle";
-import { Layout } from "./Layout";
-import { Section } from "./Section/Section";
-import { Statistics } from "./Statistics/Statistics"
-import { Notification } from "./Notification/Notification";
+import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  } 
- 
-  increment = (name) => {
-    console.log(name)
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1
-    }))
-  // if (name === 'good') {
-  //     this.setState((prevState) => ({
-  //       good: prevState.good + 1
-  //     }));
-  //   }
-  //   if (name === 'neutral') {
-  //     this.setState((prevState) => ({
-  //       neutral: prevState.neutral +1
-  //     }));
-  //   }
-  //   if (name === 'bad') {
-  //     this.setState((prevState) => ({
-  //       bad: prevState.bad + 1,
-  //     }));
-  //   }
-}
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  // console.log(good);
+  const options = ['good', 'neutral', 'bad'];
+
+  const increment = name => {
+    // console.log(name);
+
+    if (name === 'good') {
+      setGood(prevState => prevState + 1);
+    }
+    if (name === 'neutral') {
+      setNeutral(prevState => prevState + 1);
+    }
+    if (name === 'bad') {
+      setBad(prevState => prevState + 1);
+    }
+  };
+
+  const countTotalFeedback = () => {
     const totalFeedbacks = good + neutral + bad;
     return totalFeedbacks;
-  }
-  countPositiveFeedbackPercentage = () => {
-    const positivPercentage = Math.round(this.state.good / this.countTotalFeedback() * 100) || 0
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const positivPercentage =
+      Math.round((good / countTotalFeedback()) * 100) || 0;
     return positivPercentage;
-  }
+  };
 
-  render() {
-    // const {good, neutral, bad} = this.state
-    const options = Object.keys(this.state);
-    return (
+  return (
     <Layout>
-      <GlobalStyle/>
+      <GlobalStyle />
       <div>
-        <Section title = {'Please, leave feedback'}>
-           <FeedbackOptions options={options} onLeaveFeedback={this.increment}/>
+        <Section title={'Please, leave feedback'}>
+          <FeedbackOptions options={options} onLeaveFeedback={increment} />
         </Section>
-         
-        <Section title = {'Statistics'}>
-             {this.countTotalFeedback() > 0 ?
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotalFeedback}
-                positivePercentage={this.countPositiveFeedbackPercentage}
-              /> :
-              <Notification message={'There is no feedback'} />}
+
+        <Section title={'Statistics'}>
+          {countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback}
+              positivePercentage={countPositiveFeedbackPercentage}
+            />
+          ) : (
+            <Notification message={'There is no feedback'} />
+          )}
         </Section>
       </div>
-        
-   </Layout>
-    
+    </Layout>
   );
-  }
 };
